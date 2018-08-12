9dbfae373422c7d82e78015be20f77ce5b149b08
panigrahy.vivek.tmp@gmail.com <vivekananda.panigrahy@sap.com>
2018-08-12 06:38:56 -0400

    Calling API Hub OData Service

--- src/main/java/com/vivek/odatav4_sample/MyController.java
+++ src/main/java/com/vivek/odatav4_sample/MyController.java
@@ -1,7 +1,17 @@
 package com.vivek.odatav4_sample;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
+import com.sap.cloud.sdk.odatav2.connectivity.ODataQuery;
+import com.sap.cloud.sdk.odatav2.connectivity.ODataQueryBuilder;
+import com.sap.cloud.sdk.odatav2.connectivity.ODataQueryResult;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpConfigContext;
+
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.salesorder.SalesOrder;
 
 @RestController
 public class MyController{
@@ -9,5 +19,33 @@ public class MyController{
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World!!";
+	}
+	
+	
+	@GetMapping("/salesorders")
+	public List<SalesOrder> getSalesOrder() throws ODataException{
+		String destinationName = "ErpQueryEndpoint";
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
+		
+
+		ODataQuery query= ODataQueryBuilder
+		        .withEntity("/s4hanacloud/sap/opu/odata/sap/API_SALES_ORDER_SRV",
+		                "A_SalesOrder")
+		        .withHeader("apikey", "7PGAIoLeZfJzcTX1Dyt9ZpEu6bHxm0Ch", true)
+		        
+		        .select(SalesOrder.SALES_ORDER.getFieldName(), 
+		        		SalesOrder.SALES_ORDER_TYPE.getFieldName(),
+		        		SalesOrder.SALES_ORGANIZATION.getFieldName(),
+		        		SalesOrder.TOTAL_NET_AMOUNT.getFieldName(),
+		        		SalesOrder.REQUESTED_DELIVERY_DATE.getFieldName()
+		        		)
+		        .top(10)
+		        .build();
+		
+		ODataQueryResult result = query.execute(configContext);
+         
+		final List<SalesOrder> v4SalesOrders = result.asList(SalesOrder.class);
+				
+		return v4SalesOrders;
 	}
 }
